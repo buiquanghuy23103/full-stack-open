61d41a5b4eb3d868abebe4158ac0a466f80db93f
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-04-23 16:20:56 +0300

    finish anecdotes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,31 @@
 import { useState } from "react";
 
+const MostVote = ({anecdotes, votes}) => {
+	const getMostVote = () => {
+		let mostVoteIndex = 0;
+		let mostVoteValue = 0;
+		votes.forEach((vote, index) => {
+			if (vote > mostVoteValue)
+			{
+				console.log()
+				mostVoteValue = vote
+				mostVoteIndex = index
+			}
+		});
+		return { mostVoteIndex, mostVoteValue }
+	}
+
+	const mostVote = getMostVote()
+
+	return (
+		<>
+			<h1>Anecdote with most votes</h1>
+			<p>{anecdotes[mostVote.mostVoteIndex]}</p>
+			<p>has {mostVote.mostVoteValue} votes</p>
+		</>
+	)
+}
+
 const App = () => {
 	const anecdotes = [
 		'If it hurts, do it more often',
@@ -12,23 +38,27 @@ const App = () => {
 	]
 	const [selected, setSelected] = useState(0)
 	const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+	const [mostVote, setMostVote] = useState({mostVoteIndex: 0, mostVoteValue: 0})
 
 	const generateNextAnecdotes = () => {
 		const nextRandomIndex = Math.floor(Math.random() * anecdotes.length);
 		setSelected(nextRandomIndex);
 	}
-
+	
 	const incrementVote = () => {
 		const copy = [...votes]
 		copy[selected]++
 		setVotes(copy)
 	}
+
 	return (
 		<div>
+			<h1>Anecdote of the day</h1>
 			<p>{anecdotes[selected]}</p>
 			<p>has {votes[selected]} votes</p>
 			<button onClick={incrementVote}>Vote</button>
 			<button onClick={generateNextAnecdotes}>Next anecdotes</button>
+			<MostVote anecdotes={anecdotes} votes={votes} />
 		</div>
 	);
 }
