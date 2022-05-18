acdf011500b826d895b87e877e59ebed65fc63d4
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-18 09:31:49 +0300

    fix wrong config

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.NODE_ENV === 'development'
+const MONGODB_URI = process.env.NODE_ENV === 'test'
 	? process.env.MONGODB_URI
 	: process.env.TEST_MONGODB_URI
 const PORT = process.env.PORT || 3001
