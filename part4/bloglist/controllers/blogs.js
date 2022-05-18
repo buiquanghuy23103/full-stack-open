bae203004de21d727d6184a4abc03241048d0061
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-18 18:34:50 +0300

    refactor blogs controller with async/await

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,32 +1,33 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response, next) => {
-	return Blog.find({})
-		.then(blogs => {
-			return response.json(blogs)
-		})
-		.catch(error => next(error))
+blogRouter.get('/', async (request, response, next) => {
+	try {
+		const blogs = await Blog.find({})
+		return response.status(200).json(blogs)
+	} catch (error) {
+		next(error)
+	}
 })
 
-blogRouter.post('/', (request, response, next) => {
-	const newBlog = request.body
-	return Blog.create(newBlog)
-		.then(blog => {
-			return response.status(201).json(blog)
-		})
-		.catch(error => next(error))
+blogRouter.post('/', async (request, response, next) => {
+	try {
+		const savedBlog = await Blog.create(request.body)
+		return response.status(201).json(savedBlog)
+	} catch (error) {
+		next(error)
+	}
 })
 
-blogRouter.get('/:id', (request, response, next) => {
-	const requestId = request.params.id
-	return Blog.findById(requestId)
-		.then(blog => {
-			if (!blog)
-				return response.status(404).end()
-			return response.json(blog)
-		})
-		.catch(error => next(error))
+blogRouter.get('/:id', async (request, response, next) => {
+	try {
+		const id = request.params.id
+		const foundBlog = await Blog.findById(id)
+		if (!foundBlog)
+			return response.status(404).end()
+	} catch (error) {
+		next(error)
+	}
 })
 
 module.exports = blogRouter
