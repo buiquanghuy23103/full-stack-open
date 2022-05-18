71c8187d7a4dd868621b9077c1ae546d2c653b37
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-18 07:12:38 +0300

    exercise 4.5

--- part4/bloglist/utils/list_helpers.js
+++ part4/bloglist/utils/list_helpers.js
@@ -6,9 +6,15 @@ const totalLikes = (blogs) => {
 	return blogs.map(blog => blog.likes).reduce((sum, item) => sum + item, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	const max = Math.max(...blogs.map(blog => blog.likes))
+	return blogs.find(blog => blog.likes === max)
+}
+
 const helpers = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
 
 module.exports = helpers
