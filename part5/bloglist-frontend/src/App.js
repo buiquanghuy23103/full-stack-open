036ac270000c3dcd8b8f993595d5d5e479d1efde
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-23 13:20:13 +0300

    show blogs

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -6,9 +6,10 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then(blogs =>{
+		setBlogs( blogs )
+		console.log('blogs', blogs)
+	})
   }, [])
 
   return (
