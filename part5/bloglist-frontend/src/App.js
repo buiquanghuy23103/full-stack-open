7094eeacec3e6d1eb6ddb6ebb98be8f0f7830863
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-23 14:54:43 +0300

    use async/await

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -4,24 +4,27 @@ import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+	const [blogs, setBlogs] = useState([])
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>{
-		setBlogs( blogs )
-		console.log('blogs', blogs)
-	})
-  }, [])
+	const fetchBlogs = async () => {
+		const blogs = await blogService.getAll()
+		setBlogs(blogs)
+	}
 
-  return (
-    <div>
-      <h2>blogs</h2>
-	  <LoginForm />
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+	useEffect(() => {
+		fetchBlogs()
+			.catch(console.log)
+	}, [])
+
+	return (
+		<div>
+		<h2>blogs</h2>
+		<LoginForm />
+		{blogs.map(blog =>
+			<Blog key={blog.id} blog={blog} />
+		)}
+		</div>
+	)
 }
 
 export default App
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
-const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const getAll = async () => {
+	const response = await axios.get('/api/blogs')
+	return response.data
 }
 
 export default { getAll }
