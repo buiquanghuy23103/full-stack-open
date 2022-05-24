5cee4bf7a23ee9e73e0630aa39b5119aac548cec
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-24 16:43:21 +0300

    dont use Toggable

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,17 +1,30 @@
-import Toggable from "./Toggable"
+import { useState } from "react"
 
-const Blog = ({blog}) => (
-	<div className="blog">
-		{blog.title}
-		<Toggable openButtonLabel="view" closeButtonLabel="hide">
-			<p>Author: {blog.author.name}</p>
-			<p>Url: {blog.url}</p>
-			<div>
-				Likes: {blog.likes}
-				<button>like</button>
+const Blog = ({blog}) => {
+	const [showDetails, setShowDetails] = useState(false)
+
+	const toggleDetailsView = () => setShowDetails(!showDetails)
+
+	const displaySettings = {
+		display: showDetails ? '' : 'none'
+	}
+
+	return (
+		<div className="blog">
+			{blog.title}
+			<button onClick={toggleDetailsView}>
+				{ showDetails ? 'hide' : 'view' }
+			</button>
+			<div style={displaySettings}>
+				<p>Author: {blog.author.name}</p>
+				<p>Url: {blog.url}</p>
+				<div>
+					Likes: {blog.likes}
+					<button>like</button>
+				</div>
 			</div>
-		</Toggable> 
-	</div>
-)
+		</div>
+	)
+}
 
 export default Blog
