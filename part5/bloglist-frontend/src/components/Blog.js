fac09cc24cf991692bd3c6ee22d07b5579ab9bf5
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-24 15:27:27 +0300

    make blog view toggable

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,17 @@
+import Toggable from "./Toggable"
+
 const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author.name}
-  </div>  
+	<div className="blog">
+		{blog.title}
+		<Toggable openButtonLabel="view" closeButtonLabel="hide">
+			<p>Author: {blog.author.name}</p>
+			<p>Url: {blog.url}</p>
+			<div>
+				Likes: {blog.likes}
+				<button>like</button>
+			</div>
+		</Toggable> 
+	</div>
 )
 
 export default Blog
