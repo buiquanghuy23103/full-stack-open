10be1a1ae9944a38cc3426434acc9314b25e4e47
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-26 09:56:26 +0300

    test if more details are shown on click

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 describe('Default blog display', () => {
 	let blog
@@ -28,5 +29,30 @@ describe('Default blog display', () => {
 		expect(url).toBeNull()
 		const likes = screen.queryByText(blog.likes)
 		expect(likes).toBeNull()
+	})
+})
+
+describe('when blog details are shown', () => {
+	let blog
+	beforeEach(() => {
+		blog = {
+			title: 'React patterns',
+			author: {
+				username: 'edijkstra',
+				name: 'Edsger W. Dijkstra'
+			},
+			url: 'https://reactpatterns.com/',
+			likes: 7
+		}
+
+		render(<Blog blog={blog} />)
+		const user = userEvent.setup()
+		const viewButton = screen.getByRole('button', { name: /view/ })
+		user.click(viewButton)
+	})
+
+	test('should show likes and url', () => {
+		screen.getByText(blog.url, { exact: false })
+		screen.getByText(blog.likes, { exact: false })
 	})
 })
