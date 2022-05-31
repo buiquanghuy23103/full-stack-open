a0e353b58c9556ce2130ffe4d759bf31b535901a
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-05-31 22:53:23 +0300

    fetch anecdotes from server

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import { anecdoteActions } from './reducers/anecdoteReducer'
+import service from './service'
 
 const App = () => {
+	const dispatch = useDispatch()
+	useEffect(() => {
+		service.getAll()
+			.then(response => dispatch(anecdoteActions.setAnecdotes(response)))
+	}, [])
   return (
 	<div>
 	<h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const anecdotesAtStart = [
-'If it hurts, do it more often',
-'Adding manpower to a late software project makes it later!',
-'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-'Premature optimization is the root of all evil.',
-'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-	return {
-		content: anecdote,
-		id: getId(),
-		votes: 0
-	}
-}
 
 const incrementVote = (state, action) => {
 	const id = action.payload
@@ -34,14 +17,17 @@ const addAnecdote = (state, action) => {
 	state.push(newAnecdote)
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+const setAnecdotes = (state, action) => {
+	return action.payload
+}
 
 const anecdoteSlice = createSlice({
 	name: 'anecdote',
-	initialState,
+	initialState: [],
 	reducers: {
 		incrementVote,
-		addAnecdote
+		addAnecdote,
+		setAnecdotes
 	}
 })
 
