51faba7a6665dc997ac704f02a0c86449903beab
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-03 19:08:56 +0300

    create hook for communicating with backend

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -18,10 +18,9 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
-
-  const create = (resource) => {
-    // ...
+  const create = async (newResource) => {
+    const response = await axios.post(baseUrl, newResource)
+	setResources(resources.concat(response.data))
   }
 
   const service = {
@@ -41,15 +40,17 @@ const App = () => {
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
-  const handleNoteSubmit = (event) => {
+  const handleNoteSubmit = async (event) => {
     event.preventDefault()
-    noteService.create({ content: content.value })
+    await noteService.create({ content: content.value })
   }
  
-  const handlePersonSubmit = (event) => {
+  const handlePersonSubmit = async (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    await personService.create({ name: name.value, number: number.value})
   }
+
+  console.log(notes)
 
   return (
     <div>
