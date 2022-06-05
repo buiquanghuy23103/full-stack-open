24136ee55064c1ab2d261d3985085d343215e729
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-05 19:49:52 +0300

    display users' statistics

--- part7/bloglist-frontend/src/pages/Users.js
+++ part7/bloglist-frontend/src/pages/Users.js
@@ -1,9 +1,18 @@
-const Users = () => {
+import { useSelector } from 'react-redux'
+import lodash from 'lodash'
 
+const Users = () => {
+	const counts = useSelector(state => {
+		const blogs = state.blogs
+		const statistic = lodash.countBy(blogs, 'author.name')
+		return Object.entries(statistic)
+	})
+	console.log(counts)
 	return (
 		<>
 			<h2>Users</h2>
-
+			{counts.map(([author, blogCount]) =>
+				<p key={author}>{author}: {blogCount} </p>)}
 		</>
 	)
 }
