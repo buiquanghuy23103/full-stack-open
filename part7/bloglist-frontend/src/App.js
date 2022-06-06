83a75196c550f5bc5d044217378056a865980203
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-06 18:37:31 +0300

    fix import issue

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -7,7 +7,7 @@ import User from './pages/User'
 import Users from './pages/Users'
 import { fetchBlogs } from './reducers/blogReducer'
 import { userActions } from './reducers/userReducer'
-import UserInfo from '../components/UserInfo'
+import UserInfo from './components/UserInfo'
 
 const App = () => {
 	const dispatch = useDispatch()
