b624074e1679d8e341afb8e88e9af35996410f02
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-07 22:24:37 +0300

    Modify the allBooks query so that a user can give an optional parameter author.

--- part8/index.js
+++ part8/index.js
@@ -106,7 +106,7 @@ const typeDefs = gql`
 	type Query {
 		bookCount: Int!
 		authorCount: Int!
-		allBooks: [Book!]!
+		allBooks(author: String): [Book!]!
 		allAuthors: [Author!]!
   }
 `
@@ -115,7 +115,11 @@ const resolvers = {
 	Query: {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
-		allBooks: () => books,
+		allBooks: (root, args) => {
+			const authorQuery = args.author
+			if (!authorQuery) return books
+			return books.filter(b => b.author === authorQuery)
+		},
 		allAuthors: () => {
 			const result = lodash.countBy(books, 'author')
 			const arr = Object.entries(result)
