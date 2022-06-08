7b25556944865ca5a7b15b9a33ad6b5552cab56e
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-08 14:07:39 +0300

    connect Apollo client to React

--- part8/ex8b/src/index.js
+++ part8/ex8b/src/index.js
@@ -1,5 +1,16 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	uri: 'http://localhost:4000'
+})
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App/>
+	</ApolloProvider>,
+	document.getElementById('root')
+)
