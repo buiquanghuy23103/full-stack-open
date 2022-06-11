845f5651819e51389b231aab2973407fd16fde42
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-11 20:37:47 +0300

    create a basic form for login

--- part8/ex8b/src/components/Login.js
+++ part8/ex8b/src/components/Login.js
@@ -1,6 +1,29 @@
+import useField from "../useField"
+
 const Login = ({ show }) => {
+	const usernameField = useField('username', 'text')
+	const passwordField = useField('password', 'password')
+
+	const login = event => {
+		event.preventDefault()
+	}
+
 	if (!show) return null
-	return <h1>LOGIN PAGE</h1>
+	return (
+		<>
+			<h1>LOGIN</h1>
+			<form onSubmit={login}>
+				<div>
+					username
+					<input {...usernameField.inputProps}  />
+				</div>
+				<div>
+					password
+					<input {...passwordField.inputProps}  />
+				</div>
+			</form>
+		</>
+	)
 }
 
 export default Login
