305d773c90c131ae09dd4a68eff738cd284cdd02
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-12 14:25:52 +0300

    Implement a view which shows all the books based on the logged-in user's favourite genre.

--- part8/ex8b/src/components/Recommend.js
+++ part8/ex8b/src/components/Recommend.js
@@ -3,12 +3,41 @@ import queries from "../queries"
 
 const Recommend = ({ show }) => {
 	const currentUserQuery = useQuery(queries.CURRENT_USER)
+	const booksQuery = useQuery(queries.ALL_BOOKS)
 
 	if (!show) return null
-	if (currentUserQuery.loading) return <p>Loading...</p>
+	if (currentUserQuery.loading || booksQuery.loading)
+		return <p>Loading...</p>
 
 	const favouriteGenre = currentUserQuery.data.me.favouriteGenre
-	return <h1>Recommend { favouriteGenre }</h1>
+	const books = booksQuery.data.allBooks
+	console.log('books', books)
+	const filteredBooks = books.filter(b => b.genres.includes(favouriteGenre))
+	return (
+		<>
+			<h1>Recommendations</h1>
+			<p>
+				Books in your favourite genre:
+				<strong>{favouriteGenre}</strong>
+			</p>
+			<table>
+			<tbody>
+				<tr>
+					<th>Book</th>
+					<th>Author</th>
+					<th>Published</th>
+				</tr>
+				{filteredBooks.map((a) => (
+					<tr key={a.title}>
+					<td>{a.title}</td>
+					<td>{a.author.name}</td>
+					<td>{a.published}</td>
+					</tr>
+				))}
+				</tbody>
+			</table>
+		</>
+	)
 }
 
 export default Recommend
