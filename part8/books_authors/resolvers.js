71e6b0ecbc2d0a75ab349a07953cf21b2a880a59
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-14 14:20:27 +0300

    activate subscription for book adding

--- part8/books_authors/resolvers.js
+++ part8/books_authors/resolvers.js
@@ -1,6 +1,10 @@
 const Author = require('./models/Author')
 const Book = require('./models/Book')
 const User = require('./models/User')
+const { PubSub } = require('graphql-subscriptions')
+const pubsub = new PubSub()
+
+const BOOK_ADDED = 'BOOK_ADDED'
 
 const resolvers = {
 	Book: {
@@ -33,21 +37,30 @@ const resolvers = {
 		allAuthors: async () => Author.find({}),
 		me: (root, args, context) => context.user
 	},
+	Subscription: {
+		bookCreated: {
+			subscribe: () => pubsub.asyncIterator([ BOOK_ADDED ])
+		}
+	},
 	Mutation: {
 		addBook: async (root, args, context) => {
 			if (!context.user)
 				throw new AuthenticationError('invalid token')
-			const author = await Author.findOne({ name: args.author })
-			if (!author)
-				throw new UserInputError('invalid author', {
-					invalidArgs: [args.author]
-				})
-			return Book.create({ ...args, author: author.id })
-				.catch(error => {
-					throw new UserInputError(error.message, {
-						invalidArgs: args
+			try {
+				const author = await Author.findOne({ name: args.author })
+				if (!author)
+					throw new UserInputError('invalid author', {
+						invalidArgs: [args.author]
 					})
+				const book = new Book({ ...args, author: author._id })
+				await book.save()
+			} catch (error) {
+				throw new UserInputError(error.message, {
+						invalidArgs: args
 				})
+			}
+			pubsub.publish(BOOK_ADDED, { bookCreated: book })
+			return book
 		},
 		deleteBook: async (root, args) => {
 			return Book.findOneAndDelete({ title: args.title })
