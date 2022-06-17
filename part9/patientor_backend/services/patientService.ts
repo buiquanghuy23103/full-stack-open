88017ce790e636485fcb31248a9f64389ab7542a
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-17 20:03:13 +0300

    add a method to add patient

--- part9/patientor_backend/services/patientService.ts
+++ part9/patientor_backend/services/patientService.ts
@@ -1,10 +1,21 @@
 import patientData from '../data/patients.json';
-import { Patient } from '../types';
+import { NewPatient, Patient } from '../types';
+import { v1 as uuid } from 'uuid';
 
 const getPatients = () => {
 	return patientData as Array<Patient>;
 };
 
+const addPatient = (newPatient: NewPatient): Patient => {
+	const patient = {
+		...newPatient,
+		id: uuid()
+	};
+	patientData.push(patient);
+	return patient;
+};
+
 export default {
-	getPatients
+	getPatients,
+	addPatient
 };
--- part9/patientor_backend/types.ts
+++ part9/patientor_backend/types.ts
@@ -14,3 +14,4 @@ export interface Patient {
 }
 
 export type PatientWithoutSsn = Omit<Patient, 'ssn'>;
+export type NewPatient = Omit<Patient, 'id'>;
