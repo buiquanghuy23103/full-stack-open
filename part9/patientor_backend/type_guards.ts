46ae9136927fa0a1a3a2b33577a8b78cfa413e76
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-17 20:53:31 +0300

    add type guard for Gender

--- part9/patientor_backend/type_guards.ts
+++ part9/patientor_backend/type_guards.ts
@@ -1,3 +1,5 @@
+import { Gender } from "./types";
+
 export const isString = (obj: unknown): obj is string => {
 	return typeof obj === 'string'
 		|| obj instanceof String;
@@ -5,4 +7,10 @@ export const isString = (obj: unknown): obj is string => {
 
 export const isDate = (date: string) => {
 	return Boolean(Date.parse(date));
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const isGender = (obj: any): obj is Gender => {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+	return Object.values(Gender).includes(obj);
 };
