b0037495210b9fb37ac277a8c1f0fcead5a2789c
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-17 20:38:10 +0300

    add date parser

--- part9/patientor_backend/parsers.ts
+++ part9/patientor_backend/parsers.ts
@@ -1,7 +1,13 @@
-import { isString } from "./type_guards";
+import { isDate, isString } from "./type_guards";
 
 export const parseString = (obj: unknown): string => {
 	if (!obj || !isString(obj))
 		throw new Error('Incorrect or missing string');
+	return obj;
+};
+
+export const parseDate = (obj: unknown): string => {
+	if (!obj || !isString(obj) || !isDate(obj))
+		throw new Error('Invalid date format');
 	return obj;
 };
