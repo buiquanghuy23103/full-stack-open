ca65423132262aed2e2ef2b6a5087b2f2ddb6353
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-17 16:28:20 +0300

    setup route for /api/patients

--- part9/patientor_backend/index.ts
+++ part9/patientor_backend/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import diagnoseRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnoseRouter);
+app.use('/api/patients', patientRouter);
 
 const PORT = 3003;
 app.listen(PORT, () => {
