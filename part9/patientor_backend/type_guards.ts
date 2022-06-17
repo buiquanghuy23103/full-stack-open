e48f7335b346b829eb0eef6110af8cc8c56d2617
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-17 20:36:47 +0300

    add type guard for date

--- part9/patientor_backend/type_guards.ts
+++ part9/patientor_backend/type_guards.ts
@@ -2,3 +2,7 @@ export const isString = (obj: unknown): obj is string => {
 	return typeof obj === 'string'
 		|| obj instanceof String;
 };
+
+export const isDate = (date: string) => {
+	return Boolean(Date.parse(date));
+};
