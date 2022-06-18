0a22be07dcdc13e94d09d803e3c2287417708663
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-18 22:31:02 +0300

    refactor dispatch

--- part9/patientor_frontend/src/state/reducer.ts
+++ part9/patientor_frontend/src/state/reducer.ts
@@ -17,6 +17,16 @@ interface AddPatientAction extends ActionBase {
 
 export type Action = SetPatientAction | AddPatientAction;
 
+export const setPatientList = (patients: Patient[]): SetPatientAction => ({
+	type: "SET_PATIENT_LIST",
+	payload: patients
+});
+
+export const addPatient = (patient: Patient): AddPatientAction => ({
+	type: "ADD_PATIENT",
+	payload: patient
+});
+
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case "SET_PATIENT_LIST":
