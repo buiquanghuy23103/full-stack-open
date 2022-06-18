a9b270cf1d1798daeee2509498d46ee3cf2603e0
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-18 20:43:31 +0300

    find patient by id in PatientInformation

--- part9/patientor_frontend/src/PatientInformation/index.tsx
+++ part9/patientor_frontend/src/PatientInformation/index.tsx
@@ -1,8 +1,17 @@
 import { useParams } from "react-router-dom";
+import { useStateValue } from "../state";
+import { Patient } from "../types";
 
 const PatientInformation = () => {
 	const { id } = useParams<{ id: string }>();
-	return <h1>Patient Information {id}</h1>;
+	const [{ patients }] = useStateValue();
+	const foundPatient = Object.values(patients).find((patient: Patient) => {
+		return patient.id === id;
+	});
+	if (!foundPatient)
+		return <h1>Patient not found.</h1>;
+	
+	return <h1>Patient Information {foundPatient.name}</h1>;
 };
 
 export default PatientInformation;
