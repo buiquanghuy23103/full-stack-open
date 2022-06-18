54d605517f5eb17fcd02cada92e22d75de1c5c01
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-18 10:34:13 +0300

    add new types

--- part9/course-info/src/types.ts
+++ part9/course-info/src/types.ts
@@ -1,4 +1,22 @@
-export interface CoursePart {
+interface CoursePartBase {
 	name: string;
 	exerciseCount: number;
+	type: string;
 }
+
+interface CourseNormalPart extends CoursePartBase {
+	type: "normal";
+	description: string;
+}
+interface CourseProjectPart extends CoursePartBase {
+	type: "groupProject";
+	groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+	type: "submission";
+	description: string;
+	exerciseSubmissionLink: string;
+}
+
+export type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
