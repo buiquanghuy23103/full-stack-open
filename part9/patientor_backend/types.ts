f6718d3c27285fd93e1efe10b17a28f94e7ae925
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-19 18:02:28 +0300

    export some types

--- part9/patientor_backend/types.ts
+++ part9/patientor_backend/types.ts
@@ -11,7 +11,7 @@ interface BaseEntry {
 	specialist: string;
 	diagnosisCodes?: Array<Diagnosis['code']>
 }
-interface OccupationalHealthcareEntry extends BaseEntry {
+export interface OccupationalHealthcareEntry extends BaseEntry {
 	type: 'OccupationalHealthcare',
 	employerName: string;
 	sickLeave?: {
@@ -19,7 +19,7 @@ interface OccupationalHealthcareEntry extends BaseEntry {
 		endDate: string;
 	}
 }
-interface HospitalEntry extends BaseEntry {
+export interface HospitalEntry extends BaseEntry {
 	type: 'Hospital',
 	discharge: {
 		date: string;
