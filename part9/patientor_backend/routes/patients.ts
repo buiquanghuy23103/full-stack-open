1257b0959a7ec5edecbdc44fa1d3ca3598044abe
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-19 19:45:44 +0300

    add try/catch

--- part9/patientor_backend/routes/patients.ts
+++ part9/patientor_backend/routes/patients.ts
@@ -5,37 +5,57 @@ import { NewEntryRequestBody, PatientRequestBody, PatientWithoutSsn } from '../t
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-	const patients: Array<PatientWithoutSsn> = patientService.getPatients().map(({
-		id, name, dateOfBirth, gender, occupation, entries
-	}) => ({
-		id, name, dateOfBirth, gender, occupation, entries
-	}));
-	return res.send(patients);
+	try {
+		const patients: Array<PatientWithoutSsn> = patientService.getPatients().map(({
+			id, name, dateOfBirth, gender, occupation, entries
+		}) => ({
+			id, name, dateOfBirth, gender, occupation, entries
+		}));
+		return res.send(patients);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json(error);
+	}
 });
 
 router.get('/:id', (req, res) => {
-	const requestId = req.params.id;
-	const foundPatient = patientService.getPatientById(requestId);
-	if (!foundPatient)
-		return res.status(404).end();
-	else
-		return res.status(200).json(foundPatient);
+	try {
+		const requestId = req.params.id;
+		const foundPatient = patientService.getPatientById(requestId);
+		if (!foundPatient)
+			return res.status(404).end();
+		else
+			return res.status(200).json(foundPatient);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json(error);
+	}
 });
 
 router.post('/', (req, res) => {
-	const newPatient = patientService.toNewPatient(req.body as PatientRequestBody);
-	const patient = patientService.addPatient(newPatient);
-	return res.json(patient);
+	try {
+		const newPatient = patientService.toNewPatient(req.body as PatientRequestBody);
+		const patient = patientService.addPatient(newPatient);
+		return res.json(patient);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json(error);
+	}
 });
 
 router.post('/:id/entries', (req, res) => {
-	const requestId = req.params.id;
-	const newEntry = patientService.toNewEntry(req.body as NewEntryRequestBody);
-	const entry = patientService.addEntry(requestId, newEntry);
-	if (!entry)
-		return res.status(404).end();
-	else
-		return res.status(200).json(entry);
+	try {
+		const requestId = req.params.id;
+		const newEntry = patientService.toNewEntry(req.body as NewEntryRequestBody);
+		const entry = patientService.addEntry(requestId, newEntry);
+		if (!entry)
+			return res.status(404).end();
+		else
+			return res.status(200).json(entry);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json(error);
+	}
 });
 
 export default router;
