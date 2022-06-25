cf8b964fb83fb4a02f8cc78b5e8cc427e64136bb
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-25 17:52:26 +0300

    refactor FormikDiagnosisSelectField

--- part9/patientor_frontend/src/AddPatientModal/FormField.tsx
+++ part9/patientor_frontend/src/AddPatientModal/FormField.tsx
@@ -1,15 +1,9 @@
 import React, { useState } from "react";
-import { ErrorMessage, FieldProps, FormikProps } from "formik";
+import { ErrorMessage, FieldProps } from "formik";
 import {
-  Select,
-  FormControl,
-  MenuItem,
   TextField as TextFieldMUI,
   Typography,
 } from "@material-ui/core";
-import { Diagnosis } from "../types";
-import { InputLabel } from "@material-ui/core";
-import Input from '@material-ui/core/Input';
 
 /*
   for exercises 9.24.-
@@ -44,43 +38,5 @@ export const NumberField = ({ field, label, min, max }: NumberProps) => {
         <ErrorMessage name={field.name} />
       </Typography>
     </div>
-  );
-};
-
-export const DiagnosisSelection = ({
-  diagnoses,
-  setFieldValue,
-  setFieldTouched,
-}: {
-  diagnoses: Diagnosis[];
-  setFieldValue: FormikProps<{ diagnosisCodes: string[] }>["setFieldValue"];
-  setFieldTouched: FormikProps<{ diagnosisCodes: string[] }>["setFieldTouched"];
-}) => {
-  const [selectedDiagnoses, setDiagnoses] = useState<string[]>([]);
-  const field = "diagnosisCodes";
-  const onChange = (data: string[]) => {    
-    setDiagnoses([...data]);
-    setFieldTouched(field, true);
-    setFieldValue(field, selectedDiagnoses);
-  };
-
-  const stateOptions = diagnoses.map((diagnosis) => ({
-    key: diagnosis.code,
-    text: `${diagnosis.name} (${diagnosis.code})`,
-    value: diagnosis.code,
-  }));
-
-  return (
-    <FormControl style={{ width: 552, marginBottom: '30px' }}>
-      <InputLabel>Diagnoses</InputLabel>
-      <Select multiple value={selectedDiagnoses} onChange={(e) => onChange(e.target.value as string[])} input={<Input />}>
-        {stateOptions.map((option) => (
-          <MenuItem key={option.key} value={option.value}>
-            {option.text}
-          </MenuItem>
-        ))}
-      </Select>
-      <ErrorMessage name={field} />
-    </FormControl>
   );
 };
