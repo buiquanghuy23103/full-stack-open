3976c8a2cdbf48a58d29c15a0dfc0c809039c891
buiquanghuy23103 <huythdnbkltv@gmail.com>
2022-06-26 09:09:00 +0300

    add action for adding entries

--- part9/patientor_frontend/src/state/reducer.ts
+++ part9/patientor_frontend/src/state/reducer.ts
@@ -1,5 +1,5 @@
 import { State } from "./state";
-import { assertNever, Diagnosis, Patient } from "../types";
+import { assertNever, Diagnosis, Entry, Patient } from "../types";
 
 interface ActionBase {
 	type: string;
@@ -20,7 +20,18 @@ interface SetDiagnosesAction extends ActionBase {
 	payload: Diagnosis[];
 }
 
-export type Action = SetPatientAction | AddPatientAction | SetDiagnosesAction;
+interface AddEntryAction extends ActionBase {
+	type: 'ADD_ENTRY',
+	payload: {
+		patientId: string;
+		entry: Entry
+	}
+}
+
+export type Action = SetPatientAction
+	| AddPatientAction
+	| SetDiagnosesAction
+	| AddEntryAction;
 
 export const setPatientList = (patients: Patient[]): SetPatientAction => ({
 	type: "SET_PATIENT_LIST",
@@ -35,6 +46,11 @@ export const addPatient = (patient: Patient): AddPatientAction => ({
 export const setDiagnoses = (diagnoses: Diagnosis[]): SetDiagnosesAction => ({
 	type: 'SET_DIAGNOSES',
 	payload: diagnoses
+});
+
+export const addEntry = (patientId: string, entry: Entry): AddEntryAction => ({
+	type: 'ADD_ENTRY',
+	payload: { patientId, entry }
 });
 
 export const reducer = (state: State, action: Action): State => {
@@ -62,6 +78,22 @@ export const reducer = (state: State, action: Action): State => {
 		return {
 			...state,
 			diagnoses: action.payload
+		};
+	case 'ADD_ENTRY':
+		const newEntry = action.payload.entry;
+		const foundPatient = state.patients[action.payload.patientId];
+		const newPatient: Patient = {
+			...foundPatient,
+			entries: foundPatient.entries
+				? foundPatient.entries.concat(newEntry)
+				: [newEntry]
+		};
+		return {
+			...state,
+			patients: {
+				...state.patients,
+				[action.payload.patientId]: newPatient
+			}
 		};
 	default:
 		assertNever(action);
